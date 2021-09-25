d87a020048ecb08f83475f2a914bea006167bf1c
DZahar0v <dimazakharov9898@gmail.com>
2021-09-25 21:55:05 +0300

    Add controller for frontend.

--- src/services/blockchain/controller.ts
+++ src/services/blockchain/controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 
-import {getAssetsOfUser, getChartsInfo} from '../../utils/zerionFetcher';
+import {getAssetsOfUser, getChartsInfo, getProfitsInfo} from '../../utils/zerionFetcher';
 
 export const getChart = async (req: Request, res: Response, next: NextFunction) => {
 	let address: string = req.params.address;
@@ -13,5 +13,14 @@ export const getAssets = async (req: Request, res: Response, next: NextFunction)
 	let address: string = req.params.address;
 
 	let data = await getAssetsOfUser(address);
+	return res.json(data)
+};
+
+export const getProfit = async (req: Request, res: Response, next: NextFunction) => {
+	let address: string = req.params.address;
+	let txStartOffset: number = req.query.txStartOffset;
+	let txEndOffset: number = req.query.txEndOffset;
+
+	let data = await getProfitsInfo(address, txStartOffset, txEndOffset);
 	return res.json(data)
 };
--- src/services/blockchain/routes.ts
+++ src/services/blockchain/routes.ts
@@ -7,3 +7,4 @@ export const blockchainRouter = express.Router();
 
 blockchainRouter.route('/chart/:address').get(controller.getChart);
 blockchainRouter.route('/assets/:address').get(controller.getChart);
+blockchainRouter.route('/profit/:address').get(controller.getProfit);
